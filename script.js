4dcfea6a9f5bdfd5c46061aaeda48ad44a3a7ad2
Ian Drews <idrews094@gmail.com>
2018-04-21 16:37:29 -0500

    added basic opperation functions, decided on basic html for page for ease of linking js functions to buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+let add = (a, b) => {return Number(a) + Number(b)};
+let subtract = (a, b) => {return a - b};
+let multiply = (a, b) => {return a * b};
+let divide = (a, b) => {return a / b};
+let operate = (operator, a, b) => {return operator(a, b )}

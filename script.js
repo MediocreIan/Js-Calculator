f9958d3f82c54e3e04ef418ff5d555f385803366
Ian Drews <idrews094@gmail.com>
2018-04-26 13:56:10 -0500

    added operate function and tied to equals button, contains function to replace divide and x characters with * and /. added functioality to have screen reset when a number is entered after an operation is completed. added comment documentation

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-let add = (a, b) => {return Number(a) + Number(b)};
-let subtract = (a, b) => {return a - b};
-let multiply = (a, b) => {return a * b};
-let divide = (a, b) => {return a / b};
-let operate = (operator, a, b) => {return operator(a, b )}
+let fin = false;
+window.onload = function() {
+
+	let operate = () => {
+    let math = document.getElementById('screen').value;
+    math = math.replace('x', '*');
+    math = math.replace('÷', '/');
+    document.getElementById('screen').value = eval(math);
+    fin = true;
+}
+
+function reset() {
+    document.getElementById('screen').value = '';
+}
+
+function updateScreen() {
+    var buttonValue = this.textContent;
+    if (fin && !isNaN(buttonValue)) {
+    	reset()
+    }
+    document.getElementById('screen').value += buttonValue
+    fin = false;
+}
+    let maths = document.getElementsByClassName("print");
+    for (var i = 0; i < maths.length; i++) {
+        maths[i].addEventListener('click', updateScreen);
+    }
+    document.getElementById('clear').addEventListener('click', reset)
+    document.getElementById('equals').addEventListener('click', operate)
+}
+/*var operate = {
+    '+': function (x, y) { return x + y },
+    '-': function (x, y) { return x - y },
+    '*': function (x, y) { return x * y },
+    '/': function (x, y) { return x / y }
+}​​​​​ possible alternate method of running math in the screen */
